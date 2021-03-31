8a9d81e8b1089cbf04f1fb64388dc111a6453912
Andrus Rinde <andrusrinde@users.noreply.github.com>
2021-03-31 11:13:49 +0300

    kahesas tund tehtud

--- tund_8/javascript/script1.js
+++ tund_8/javascript/script1.js
@@ -13,7 +13,8 @@ window.onload = function(){
 	putOpeningTime();
 	putRandomPic();
 	document.getElementById("tlu_pic").addEventListener("click", putRandomPic);
-	clockTick();
+	//järgnev on failis clock8.js
+	init_clock();
 	//let timer = setInterval(clockTick, 500); //clearInterval(timer);
 	document.getElementById("nextphoto").addEventListener("click", nextPhoto);
 	document.getElementById("prevphoto").addEventListener("click", prevPhoto);
@@ -60,20 +61,4 @@ function putOpeningTime(){
 	let currentminute = currenttime.getMinutes();
 	let currentsecond = currenttime.getSeconds(); 
 	document.getElementById("timeplace").innerHTML = "Leht avati kell: " + currenthour +":" + currentminute + ":" + currentsecond +".";
-}
-
-function clockTick(){
-	let currenttime = new Date();
-	let currenthour = currenttime.getHours();
-	let currentminute = currenttime.getMinutes();
-	let currentsecond = currenttime.getSeconds();
-	let secangle = currentsecond * 6;
-	let minangle = currentminute * 6 + currentsecond * .1;
-	let hourangle = currenthour * 30 + currentminute * .5;
-	//transform: rotate(45deg)
-	document.getElementById("secondhand").style.transform = "rotate(" + secangle + "deg)";
-	document.getElementById("minutehand").style.transform = "rotate(" + minangle + "deg)";
-	document.getElementById("hourhand").style.transform = "rotate(" + hourangle + "deg)";
-	//setTimeout(clockTick, 1000);
-	requestAnimationFrame(clockTick);
 }
